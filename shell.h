6667facc8b944f20dbb0ed2e86a6228ebbf61243
Parlog07 <127505288+Parlog07@users.noreply.github.com>
2023-09-21 01:13:19 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,8 +1,15 @@
 #ifndef _SHELL_
 #define _SHELL_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+/*main.c*/
+void executeCMD(char *command, char *argv[], char *envp[]);
 
 #endif
