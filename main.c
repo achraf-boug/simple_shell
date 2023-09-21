af67ae70c60c23636a9dabd3e4b4f897e85d231e
Achraf <108756460+achraf-boug@users.noreply.github.com>
2023-09-21 01:14:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include "shell.h"
 
 /**
+ * executeCMD - execute the commands
+ * @command: command to execute
+ * @argv: list of arguments
+ * @envp: env params
+ *
+ * Return: void
+ */
+void executeCMD(char *command, char *argv[], char *envp[])
+{
+		pid_t pid;
+		int status;
+		/*Fork a child process*/
+		pid = fork();
+
+		if (pid == -1)
+			perror("Error fork");
+		else if (pid == 0)
+		{
+			/*Child process*/
+			if (access(command, X_OK) == 0)
+			{
+				if (execve(command, argv, envp) == -1)
+				{
+					perror(command);
+					_exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				printf("%s: No such file or directory\n", command);
+				_exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			/*Parent process*/
+			if (waitpid(pid, &status, 0) == -1)
+				perror("waitpid");
+		}
+}
+
+/**
  * main - run shell
  * @argc: number of arguments
  * @argv: list of arguments
@@ -20,6 +62,14 @@ int main(int argc, char *argv[], char *envp[])
 		fflush(stdout);
 
 		chars = getline(&buffer, &bufsize, stdin);
+		if (chars == -1)
+		{
+			/*Handle the "end of file" condition*/
+			printf("\n");
+			break;
+		}
+
+		/*Remove '\n'*/
 		if (chars > 0 && buffer[chars - 1] == '\n')
 			buffer[chars - 1] = '\0';
 		else
@@ -27,17 +77,9 @@ int main(int argc, char *argv[], char *envp[])
 			printf("Error reading input!\n");
 			exit(EXIT_FAILURE);
 		}
-
-		if (access(buffer, X_OK) == 0)
-		{
-			argv[0] = buffer;
-			if (execve(buffer, argv, envp) == -1)
-				perror("./hsh");
-		}
-		else
-			printf("./hsh: No such file or directory\n");
+		executeCMD(buffer, argv, envp);
 	}
 
 	free(buffer);
-	return (argc - 1);
+	return (argc);
 }
